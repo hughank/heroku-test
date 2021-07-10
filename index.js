19fb15ae7078fd0f1f740a0f089394874107eb74
Hughan Kleintjes <hughank@gmail.com>
2021-07-10 10:41:09 +0200

    Adding new request /data

--- index.js
+++ index.js
@@ -5,4 +5,11 @@ const app = express()
 
 app.get('/test', (req, res) => res.send('Awesome!!'))
 
+app.get('/data', (req, res) => {
+    res.json({
+        id: 1,
+        name: 'Coolname'
+    })
+})
+
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
